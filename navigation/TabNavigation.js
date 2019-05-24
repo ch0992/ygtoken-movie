61742467b6f3e5f78e1e216aaabf1126c7392bde
ch0992 <choilee6@naver.com>
2019-05-24 12:22:19 +0900

    TabBarNagation declare

--- navigation/TabNavigation.js
+++ navigation/TabNavigation.js
@@ -1,13 +1,57 @@
+import React from 'react'
+import { Platform } from 'react-native';
 import {createBottomTabNavigator, createAppContainer} from "react-navigation";
 import MoviesScreen from "../screens/Movies";
 import TVScreen from "../screens/TV";
 import SearchScreen from "../screens/Search";
+import { BG_COLOR } from "../constants/Colors";
+import TabBarIcon from "../components/TabBarIcon";
 
 
-const TabNavigation = createBottomTabNavigator({
-    Movies : MoviesScreen,
-    TV : TVScreen,
-    Search : SearchScreen
-});
+const TabNavigation = createBottomTabNavigator(
+    {
+        Movie :{
+            screen : MoviesScreen,
+            navigationOptions : {
+                TabBarIcon: ({focused}) => (
+                    <TabBarIcon
+                        focused={focused}
+                        name={Platform.OS === "ios" ? "ios-film" : "md-film"}
+                    />
+                )
+            }
+        },
+        TV :{
+            screen : TVScreen,
+            navigationOptions : {
+                TabBarIcon : ({focused}) => (
+                    <TabBarIcon
+                        focused={focused}
+                        name={Platform.OS === "ios" ? "ios-film" : "md-film"}
+                    />
+                )
+            }
+        }, 
+        Search : {
+            screen : SearchScreen,
+            navigationOptions : {
+                TabBarIcon : ({focused}) => (
+                    <TabBarIcon
+                        focused = {focused}
+                        name={Platform.OS === "ios" ? "ios-film" : "md-film"}
+                    />
+                )
+            }
+        }
+    },
+    {
+        tabBarOptions : {
+            showLabel : false,
+            style : {
+                backgroundColor : BG_COLOR
+            }
+        }
+    }
+);
 
 export default createAppContainer(TabNavigation);
